f2a05cac2e6acf0a473498e745dd93f6ec7a84ea
AdrianaDimitrova <a07308160@gmail.com>
2025-11-23 10:39:03 +0200

    Create a project

---
+++ P01 - homework/P03-Nums1T20/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System;
+public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
---
+++ P02-Expression/P01 - homework/P06-SquareOfNStars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
+        {
+            for (int a =0; i < N; a++)
+            {
+                Console.Write("*");
+                break;
+            }
+
+            for (int a = 0; i < N; a++)
+            {
+                Console.Write("*");
+                break;
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
+
