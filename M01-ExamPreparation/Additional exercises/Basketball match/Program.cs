c9913c5471777ce628ab9c17c342dabc14a8bc98
AdrianaDimitrova <a07308160@gmail.com>
2025-12-31 18:08:18 +0200

    .

---
+++ M01-ExamPreparation/Additional exercises/Basketball match/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string sector = Console.ReadLine();
+        int rows = int.Parse(Console.ReadLine());
+        int seats = int.Parse(Console.ReadLine());
+
+        double sectorAPrice = 11.6;
+        double sectorBPrice = 14.5;
+        double sectorCPrice = 16.10;
+        double sectorDPrice = 8.40;
+
+        double totalPrice = 0;
+
+        int totalSeats = seats * rows;
+
+        if (sector == "Sector A")
+        {
+            totalPrice = totalSeats * sectorAPrice;
+            Console.WriteLine($"{totalPrice:f2}  lv.");
+        }
+
+        else if (sector == "Sector B")
+        {
+            totalPrice = totalSeats * sectorBPrice;
+            Console.WriteLine($"{totalPrice:f2} lv.");
+        }
+
+        else if (sector == "Sector C")
+        {
+            totalPrice = totalSeats * sectorCPrice;
+            Console.WriteLine($"{totalPrice:f2} lv.");
+        }
+
+        else if (sector == "Sector D")
+        {
+            totalPrice = totalSeats * sectorDPrice;
+            Console.WriteLine($"{totalPrice:f2} lv. ");
+        }
+       
+
+
+    }
+}
+
---
+++ M01-ExamPreparation/Additional exercises/Christmas presents/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int children = int.Parse(Console.ReadLine());
+        double lego = 0;
+        double monopoly = 0;
+        double puzzle = 0;
+        double robot = 0;
+
+        for (int i = 1; i <= children; i++)
+        {
+            string present = Console.ReadLine();
+
+            if (present == "lego")
+            {
+                lego++;
+            }
+            if (present == "monopoly")
+            {
+                monopoly++;
+            }
+            if (present == "puzzle")
+            {
+                puzzle++;
+
+            }
+            if (present == "robot")
+            {
+                robot++;
+            }
+            
+
+        }
+        Console.WriteLine($"Lego {lego / children * 100:f2}% ");
+        Console.WriteLine($"Monopoly {monopoly / children *100:f2}%");
+        Console.WriteLine($"Puzzles {puzzle / children * 100:f2}%");
+        Console.WriteLine($"Robots {robot / children * 100:f2} %");
+
+    }
+}
+
