c12e947b89f28397e81e64326b319caef1c54b26
AdrianaDimitrova <a07308160@gmail.com>
2026-05-26 15:57:58 +0300

    Homework for books

---
+++ M03-OOP/Homework/Homework/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+
+public class Book
+{
+    private string title;
+    private string author;
+    private int pages;
+
+     public string Title
+    {
+        get { return title; }
+        set { title = value; }
+    }
+
+    public string Author
+    {
+        get { return author; }
+        set { author = value; }
+    }
+
+    public int Pages
+    {
+        get { return pages; }
+        set 
+        {
+            if (value > 0)
+            {
+                pages = value;
+            }
+
+            else
+            {
+                throw new ArgumentException("Страниците трябва да бъдат положително число!");
+            }
+        }
+    }
+
+    public void Print()
+    {
+        
+        Console.WriteLine($"{Title} - {Author} ({Pages} стр.)");
+    }
+}
+
+
---
+++ M03-OOP/Homework/Homework/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    static void Main()
+    {
+        int booksCount = int.Parse(Console.ReadLine()!);
+
+        List<Book> books = new List<Book>();
+
+        for (int i = 0; i < booksCount; i++)
+        { 
+            string[] bookInfo = Console.ReadLine()!.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            Book book = new Book();
+
+            book.Title = bookInfo[0];
+            book.Author = bookInfo[1];
+            book.Pages = int.Parse(bookInfo[2]);
+
+            books.Add(book);
+
+        }
+
+        Console.WriteLine("Книги: ");
+        foreach (Book book in books)
+        {
+            book.Print();
+        }
+    }
+}
+
